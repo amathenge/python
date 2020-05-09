8f6677a6c506c45b7c48feecf7c56590f97636bf
Andrew Mathenge <mathenge@gmail.com>
2020-05-09 19:01:37 -0400

    Template for Flask programming 09MAY2020 @ 19:01

---
+++ bin/a.bat
@@ -0,0 +1,1 @@
+.\bin\Scripts\activate
---
+++ bin/templates/routes.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from application import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template("index.html")
